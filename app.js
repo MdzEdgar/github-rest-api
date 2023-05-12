ad2c80c764a13d9ac0fbfa22a98ad00cc2019e47
Edgar Méndez <Edgar Mndez>
2023-05-11 21:51:53 -0500

    Anotaciones

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const path = `/users/${USERNAME}/repos`
 axios.get(`${HOSTNAME}${path}`)
   .then((res) => {
     console.log(res.data)
+    // res.data.sort()
+    // res.data.split(0, 10)
+    //console.log(res.data)
 
   })
   .catch((err) => {
